1e62bc62ca1ab737b6af4103f36633b896c4035d
shiven217 <shivender.kvs@gmail.com>
2020-10-05 21:00:28 +0530

    Updates

---
+++ precache-manifest.426ed39d903745df48216275e75266b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "466ba0fcf8ed7ee0792b731de60c7e78",
+    "url": "/RecipeApp-ReactJs/index.html"
+  },
+  {
+    "revision": "3cbc8e5eaec42a383303",
+    "url": "/RecipeApp-ReactJs/static/css/main.cf38b657.chunk.css"
+  },
+  {
+    "revision": "4195d79b50fd3ff980ef",
+    "url": "/RecipeApp-ReactJs/static/js/2.ee793320.chunk.js"
+  },
+  {
+    "revision": "1937a0fc6634269e5f47f8181e27fdc7",
+    "url": "/RecipeApp-ReactJs/static/js/2.ee793320.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3cbc8e5eaec42a383303",
+    "url": "/RecipeApp-ReactJs/static/js/main.679ff21b.chunk.js"
+  },
+  {
+    "revision": "5acc56689bba772d76ed",
+    "url": "/RecipeApp-ReactJs/static/js/runtime-main.3b80007f.js"
+  }
+]);
